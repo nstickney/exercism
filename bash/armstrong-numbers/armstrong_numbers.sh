4d957ca8d954c2d32318bd4107a99a5bec556881
Stick <nstickney@pm.me>
2018-09-28 21:08:46 -0400

    add: armstrong numbers solution

---
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Bash strict mode (https://github.com/alphabetum/bash-boilerplate)
+set -eEuo pipefail
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+IFS=$'\n\t'
+
+main() {
+	num="$1"
+	exp=${#num}
+	sum=0
+	for ((i=0; i<exp; i++)); do
+		digit=${num:$i:1}
+		sum=$((sum+digit**exp)) 
+	done
+	if [ "$sum" -eq "$num" ]; then
+		printf '%s\n' "true"
+	else
+		printf '%s\n' "false"
+		exit 1
+	fi
+}
+
+main "$@"
