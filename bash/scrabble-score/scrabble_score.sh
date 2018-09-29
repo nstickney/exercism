f8c7a2f557753308fc7216a48a3c98bef9815138
Stick <nstickney@pm.me>
2018-09-28 21:24:24 -0400

    add: started scrabble score

---
+++ bash/scrabble-score/scrabble_score.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Bash strict mode (https://github.com/alphabetum/bash-boilerplate)
+set -eEuo pipefail
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+IFS=$'\n\t'
+
+main() {
+	true
+}
+
+main "$@"
