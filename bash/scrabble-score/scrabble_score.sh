38ebcb38c751a66ecdbf8115aa83c1466426acaf
Stick <nstickney@pm.me>
2018-09-29 20:14:03 -0400

    add: solved scrabble score

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -6,7 +6,35 @@ trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 IFS=$'\n\t'
 
 main() {
-	true
+	word=$1
+
+	local ones=AaEeIiOoUuLlNnRrSsTt
+	local twos=DdGg
+	local threes=BbCcMmPp
+	local fours=FfHhVvWwYy
+	local fives=Kk
+	local eights=JjXx
+	local tens=QqZz
+
+	local score=0
+	for (( i=0; i<${#word}; i++ )); do
+		if [[ $ones = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 1 ))
+		elif [[ $twos = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 2 ))
+		elif [[ $threes = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 3))
+		elif [[ $fours = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 4 ))
+		elif [[ $fives = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 5 ))
+		elif [[ $eights = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 8 ))
+		elif [[ $tens = *"${word:$i:1}"* ]]; then
+			score=$(( "$score" + 10 ))
+		fi
+	done
+	echo $score
 }
 
 main "$@"
