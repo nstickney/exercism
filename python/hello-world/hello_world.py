00bd6e2f1571c17eb232e0dd99c4832a14615a2c
Stick <nstickney@pm.me>
2018-09-30 17:11:08 -0400

    add: started Python track, solved Hello World

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Hello, World!
+"""
+
+__author__ = "Stick"
+__version__ = "0.1.0"
+__license__ = "Unlicense"
+
+
+def hello():
+    """ Hello, World! """
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    print(hello())
