043634459a097752842bc61de716cd87cd410d4a
Stick <nstickney@pm.me>
2018-10-01 13:21:19 -0400

    add: solved reverse string (extended slicing)

---
+++ python/reverse-string/reverse_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Reverse a string (trivial in python) 
+"""
+
+__author__ = "Stick"
+__version__ = "0.1.0"
+__license__ = "Unlicense"
+
+
+import sys
+
+
+def reverse(text):
+    """ Reverse a string (trivial in python) """
+    return text[::-1]
+
+
+if __name__ == "__main__":
+    print(reverse(sys.argv[1]))
