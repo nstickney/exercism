c2a65442d22c56b3cbe0026443c3479d2f1ab70f
Stick <nstickney@pm.me>
2018-10-01 13:50:15 -0400

    add: solved gigasecond

---
+++ python/gigasecond/gigasecond.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Calculate the moment when someone has lived for 10^9 seconds.
+"""
+
+__author__ = "Stick"
+__version__ = "0.1.0"
+__license__ = "Unlicense"
+
+
+from datetime import datetime, timedelta
+from sys import argv
+
+
+def add_gigasecond(birth_date):
+    """ Calculate the moment when someone has lived for 10^9 seconds """
+    return birth_date + timedelta(seconds=1000000000)
+
+
+if __name__ == "__main__":
+    print(add_gigasecond(datetime(int(argv[1]), int(argv[2]), int(argv[3]))))
