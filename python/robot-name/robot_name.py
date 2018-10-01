9c3e2221f88f39b244b403ba403ac323c1f6e7ae
Stick <nstickney@pm.me>
2018-09-30 21:11:07 -0400

    add: solved robot name

---
+++ python/robot-name/robot_name.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Manage robot factory settings.
+"""
+
+__author__ = "Stick"
+__version__ = "0.1.0"
+__license__ = "Unlicense"
+
+
+from random import randint as ri
+
+
+class Robot:
+    """ Factory robots with different names """
+
+    robots = list()
+
+    def __init__(self):
+        self.name = self.generate_name()
+        self.robots.append(self.name)
+
+    def generate_name(self):
+        """ Randomly generate an available name of the form AA000 """
+        name = ""
+        while name == "" or name in self.robots:
+            name = chr(ri(65, 91)) + chr(ri(65, 91)) + str(ri(100, 1000))
+        return name
+
+    def reset(self):
+        """ Reset a robot to factory defaults (and a new name) """
+        old_name = self.name
+        self.name = self.generate_name()
+        self.robots.remove(old_name)
