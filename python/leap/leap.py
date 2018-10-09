d5368a581e54a5c8a9d54a422b9d6a8e6aa623e6
Stick <nstickney@pm.me>
2018-10-09 11:12:06 -0400

    fix: leap(.py)ing faster

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -4,7 +4,7 @@ Determine whether a year is a leap year or not
 """
 
 __author__ = "Stick"
-__version__ = "0.1.0"
+__version__ = "0.1.3"
 __license__ = "Unlicense"
 
 
@@ -13,7 +13,7 @@ import sys
 
 def is_leap_year(year):
     """ Determine whether a year is a leap year or not """
-    return (year % 400 == 0) or (year % 4 == 0 and not year % 100 == 0)
+    return year % 4 == 0 and (not year % 100 == 0 or year % 400 == 0)
 
 
 if __name__ == "__main__":
