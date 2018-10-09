f24668b14adb60426162141983a5d8f6da74b659
Stick <nstickney@pm.me>
2018-10-09 13:29:20 -0400

    add: solved bob (python)

---
+++ python/bob/bob.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Determine whether a year is a leap year or not
+"""
+
+__author__ = "Stick"
+__license__ = "Unlicense"
+
+
+from sys import argv
+
+
+def hey(phrase):
+    phrase = phrase.strip()
+    if phrase == "":
+        return "Fine. Be that way!"
+    if phrase.endswith("?"):
+        if phrase.isupper() and not phrase.islower():
+            return "Calm down, I know what I'm doing!"
+        return "Sure."
+    if phrase.isupper() and not phrase.islower():
+        return "Whoa, chill out!"
+    return "Whatever."
+
+
+def main(phrase):
+    print(hey(phrase))
+
+
+def print_usage():
+    print("Usage: python[3]", argv[0], "<phrase>")
+    print("    phrase: something to say to Bob")
+    exit(1)
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print_usage()
+    main(argv[1])
