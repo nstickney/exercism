c101aaaedcf45eb9704963d9899c49f158e940c4
Stick <nstickney@pm.me>
2018-10-21 14:13:23 -0400

    fix: remove extra checks from bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -11,23 +11,26 @@ from sys import argv
 
 
 def hey(phrase):
+    """ Say something to bob """
     phrase = phrase.strip()
     if phrase == "":
         return "Fine. Be that way!"
     if phrase.endswith("?"):
-        if phrase.isupper() and not phrase.islower():
+        if phrase.isupper():
             return "Calm down, I know what I'm doing!"
         return "Sure."
-    if phrase.isupper() and not phrase.islower():
+    if phrase.isupper():
         return "Whoa, chill out!"
     return "Whatever."
 
 
 def main(phrase):
+    """ Run the bob program """
     print(hey(phrase))
 
 
 def print_usage():
+    """ Print a helpful usage message """
     print("Usage: python[3]", argv[0], "<phrase>")
     print("    phrase: something to say to Bob")
     exit(1)
