67b88509ee7af76d6dded0b72dc29867fcd353c4
Stick <nstickney@pm.me>
2018-10-22 20:25:55 -0400

    fix: break out allergy logic

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -19,12 +19,16 @@ class Allergies:
     def __init__(self, score):
         self.score = score
 
+    def is_allergic_by_index(self, index):
+        """ Determine if score represents allergy to an item given by index """
+        return bool(self.score >> index & 1)
+
     def is_allergic_to(self, item):
         """ Determine if the score represents an allergy to a specific item """
-        return bool(self.score >> self.allergens.index(item) & 1)
+        return self.is_allergic_by_index(self.allergens.index(item))
 
     @property
     def lst(self):
         """ Determine the list of allergens the allergy score represents """
         return [self.allergens[i] for i in range(len(self.allergens))
-                if self.score >> i & 1]
+                if self.is_allergic_by_index(i)]
