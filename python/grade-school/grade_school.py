35a37e61e92a51d34e937aa364d4944405dbdcdd
Stick <nstickney@pm.me>
2018-12-05 11:08:14 -0500

    add: solved Grade School (python)

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,25 @@
+def sort_dict_by_keys(d):
+    result = {}
+    for key in sorted(d.keys()):
+        result[key] = d[key]
+    return result
+
+
 class School(object):
     def __init__(self):
-        pass
+        self.grades = {}
 
     def add_student(self, name, grade):
-        pass
+        if grade not in self.grades:
+            self.grades[grade] = []
+            self.grades = sort_dict_by_keys(self.grades)
+        self.grades[grade].append(name)
+        self.grades[grade].sort()
 
     def roster(self):
-        pass
+        return [student for grade in self.grades.values() for student in grade]
 
     def grade(self, grade_number):
-        pass
+        if grade_number in self.grades:
+            return self.grades[grade_number]
+        return []
